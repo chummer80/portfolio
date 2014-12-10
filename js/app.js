c391c45ddf294c3ec9106b664dad551d549b0475
chummer80 <mike.c.chu@gmail.com>
2014-12-09 19:31:02 -0800

    revert merge commit
    it was an accident

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     /******************
      * CONSTANTS
      *******************/
-	var DEBUG = false;
+    var DEBUG = true;
 
     var COLORS = {
         WHITE: {
@@ -337,25 +337,23 @@ $(document).ready(function() {
     carousel.getJqueryObj().insertAfter($('#title_div + div.flex_spacer'));
 
 
-    // make sure images are loaded before creating the arrow buttons
-    $(window).load(function() {
-        // put arrow button images on the canvases
-        var controlDivHeight = $('#carousel_controls').innerHeight();
-        for (var side in arrowData) {
-            var img = $('#arrow_button_' + side + '_image').get(0);
 
-            // scale canvas and image to fit height of the controls container
-            var scale = controlDivHeight / img.height;
-            arrowData[side].canvas = $('#arrow_button_' + side).get(0);
-            arrowData[side].canvas.width = img.width * scale;
-            arrowData[side].canvas.height = img.height * scale;
-            arrowData[side].context = arrowData[side].canvas.getContext('2d');
+    // put arrow button images on the canvases
+    var controlDivHeight = $('#carousel_controls').innerHeight();
+    for (var side in arrowData) {
+        var img = $('#arrow_button_' + side + '_image').get(0);
 
-            // draw image on canvas
-            arrowData[side].context.drawImage(img, 0, 0, arrowData[side].canvas.width, arrowData[side].canvas.height);
+        // scale canvas and image to fit height of the controls container
+        var scale = controlDivHeight / img.height;
+        arrowData[side].canvas = $('#arrow_button_' + side).get(0);
+        arrowData[side].canvas.width = img.width * scale;
+        arrowData[side].canvas.height = img.height * scale;
+        arrowData[side].context = arrowData[side].canvas.getContext('2d');
 
-            // store original pixel data so color can be changed later
-            arrowData[side].originalPixels = arrowData[side].context.getImageData(0, 0, arrowData[side].canvas.width, arrowData[side].canvas.height);
-        }
-    });
+        // draw image on canvas
+        arrowData[side].context.drawImage(img, 0, 0, arrowData[side].canvas.width, arrowData[side].canvas.height);
+
+        // store original pixel data so color can be changed later
+        arrowData[side].originalPixels = arrowData[side].context.getImageData(0, 0, arrowData[side].canvas.width, arrowData[side].canvas.height);
+    }
 });
